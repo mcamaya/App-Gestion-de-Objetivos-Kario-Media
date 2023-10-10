02ff3e87a576b433ee40f2b5c7024d0a2b6bb8bf
MDEstrada314 <mdelprograma@gmail.com>
2023-10-10 17:13:40 -0500

    fix: :sparkles: nav arreglado
    por fin es reposibe esa vaina

--- app/frontend/src/components/navbar.jsx
+++ app/frontend/src/components/navbar.jsx
@@ -1,52 +1,106 @@
 import React from "react";
 import "./css/nav.css";
 
-import logo from  '../assets/img/kario.png'
-import mas from  '../assets/img/mas.svg'
-import refres from  '../assets/img/res.svg'
-import eliminar from  '../assets/img/borrar.svg' 
-import repor from  '../assets/img/error.svg'
-import ayuda from  '../assets/img/question.svg'
+import logo from "../assets/img/kario.png";
+import mas from "../assets/img/mas.svg";
+import refres from "../assets/img/res.svg";
+import eliminar from "../assets/img/borrar.svg";
+import repor from "../assets/img/error.svg";
+import ayuda from "../assets/img/question.svg";
+import campana from "../assets/img/campana.svg";
+import ajustes from "../assets/img/ajustes.svg";
+import imgen from "../assets/img/user-image.jpg";
 
-
-export default function Navbar(){
-    return(
-        <div className="d-flex align-items-center vw-100">
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                <div className="container-fluid">
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav mx-auto ">
-                    <li className="nav-item">
-                    <a className="nav-link active grup-a d-flex justify-content-center align-items-center gap-3" aria-current="page" href="#"><img className="nav-logo ms-3"  src={mas} alt="" />Añadir</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link active grup-a d-flex justify-content-center align-items-center gap-3" aria-current="page" href="#"><img className="nav-logo" src={refres} alt="" />Refrescar</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link active grup-a d-flex justify-content-center align-items-center gap-3" aria-current="page" href="#"><img className="nav-logo" src={eliminar} alt="" />Eliminar</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-item" href="#"><img src={logo} className="logo-nav" alt="" srcset="" /></a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link active grup-a d-flex justify-content-center align-items-center gap-3" aria-current="page" href="#"><img className="nav-logo" src={repor} alt="" />Eliminar</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link active grup-a d-flex justify-content-center align-items-center gap-3" aria-current="page" href="#"><img className="nav-logo" src={ayuda} alt="" />Ayuda</a>
-                    </li>
+export default function Navbar() {
+  return (
+    <div>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a
+                  class="nav-link active grup-a d-flex align-items-center gap-2"
+                  aria-current="page"
+                  href="#"
+                >
+                  <img className="nav-logo" src={mas} alt="" /> Agregar
+                </a>
+              </li>
+              <li class="nav-item ">
+                <a
+                  class="nav-link active grup-a d-flex align-items-center gap-2 "
+                  aria-current="page"
+                  href="#"
+                >
+                  <img className="nav-logo" src={refres} alt="" /> Referescar
+                </a>
+              </li>
+              <li class="nav-item">
+                <a
+                  class="nav-link active grup-a d-flex align-items-center gap-2"
+                  aria-current="page"
+                  href="#"
+                >
+                  <img className="nav-logo" src={eliminar} alt="" /> Eliminar
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active grup-a" aria-current="page" href="#">
+                  <img className="logo-nav " src={logo} alt="" />
+                </a>
+              </li>
+              <li class="nav-item">
+                <a
+                  class="nav-link active grup-a d-flex align-items-center gap-2"
+                  aria-current="page"
+                  href="#"
+                >
+                  <img className="nav-logo" src={repor} alt="" /> Reportar
+                </a>
+              </li>
+              <li class="nav-item">
+                <a
+                  class="nav-link active grup-a d-flex align-items-center gap-2"
+                  aria-current="page"
+                  href="#"
+                >
+                  <img className="nav-logo" src={ayuda} alt="" /> Ayuda
+                </a>
+              </li>
             </ul>
-            <form class="d-flex" role="search">
-                <img src="" alt="" />
-                <h1>hola</h1>
-             <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
-            </div>
+
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0 d-flex align-items-center">
+              <li class="nav-item ">
+                <a class="nav-link active" aria-current="page" href="#">
+                  <img className="ajustes-nav" src={ajustes} alt="" />
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active " aria-current="page" href="#">
+                  <img className="ajustes-nav" src={campana} alt="" />
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active " aria-current="page" href="#">
+                  <img className="imagen-nav" src={imgen} alt="" />
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-</nav>
-           
-        </div>
-    )
+      </nav>
+    </div>
+  );
 }
--- app/frontend/src/components/panel.jsx
+++ app/frontend/src/components/panel.jsx
@@ -1,19 +1,52 @@
-import React from 'react';
+import React from "react";
+import '../components/css/panel.css'
 
-export default function Panel(){
-    return(
-        <div>
-            <div>
-                <div>
-                    <h1>Panel de Indicadores</h1>
-                </div>
-                <div>
-                    <p>Aqui puedes visualizar los indicadores propuestos y añadidos por tu equipo de trabajo. Si quieres ver más detalles , dale click a uno de ellos para más información</p>
-                </div>
-            </div>
-            <div>
-                
-            </div>
+export default function Panel() {
+  return (
+    <div>
+      <div>
+        <div className="d-flex justify-content-center mt-3">
+          <h1>Panel de Indicadores</h1>
         </div>
-    )
+        <div className="d-flex justify-content-center">
+          <p>
+            Aqui puedes visualizar los indicadores propuestos y añadidos por tu
+            equipo de trabajo. Si quieres ver más detalles , dale click a uno de
+            ellos para más información
+          </p>
+        </div>
+      </div>
+      <div className="p-5">
+        <table>
+          <thead>
+            <tr className="table">
+              <th scope="col">#</th>
+              <th scope="col">First</th>
+              <th scope="col">Last</th>
+              <th scope="col">Handle</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="m-5 tabla ">
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td colspan="2">Larry the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
