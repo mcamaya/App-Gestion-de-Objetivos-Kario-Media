021ebb83e4795dc36ac84687a4729eee5a186d7b
jogebro <joelabril2110@gmail.com>
2023-10-10 11:27:55 -0500

    feat: :sparkles: welcome responsive PC

--- app/frontend/src/components/saludo.jsx
+++ app/frontend/src/components/saludo.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Link } from 'react-router-dom';
+import React, {useEffect} from 'react';
+import { BrowserRouter as Link, useHistory } from 'react-router-dom';
 import './css/saludo.css'
 import logo2 from '../assets/img/kario-media.png';
 import logo from '../assets/img/kario.png';
 import userImage from '../assets/img/user-image.jpg';
 
 export default function Saludo(){
+    /* const history = useHistory();
+
+    useEffect(()=>{
+        const timer = setTimeout(()=>{
+            history.push('/home')
+        }, 4000)
+
+        return () => clearTimeout(timer);
+    }, [history]) */
+
     return (
         <div className='container'>
             <img id="img-animated" src={logo} alt="..."/>
@@ -14,7 +24,7 @@ export default function Saludo(){
                     <img src={logo2} alt="..."/>
                 </div>
                 <div className='bienvenida-loged'>
-                    <h1>Bienvenido de vuelta</h1>
+                    <h1>Bienvenido de nuevo</h1>
                 </div>
                 <div className="user-loged">
                     <img src={userImage} alt="..."/>
