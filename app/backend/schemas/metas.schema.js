5627cf1193e828546fd7ac2fcdf030711c74dd96
Nightshadow7 <hernandezdiazanthonilexandre@gmail.com>
2023-10-11 11:52:47 -0500

    docs :memo: Update swagger Se hizo una actualizacion de la documentacion del Swagger

--- app/backend/schemas/metas.schema.js
+++ app/backend/schemas/metas.schema.js
@@ -1,7 +1,7 @@
 import Joi from "joi";
 
 const id = Joi.string().hex().length(24);
-const nombre = Joi.string().min(8).max(20);
+const nombre = Joi.string().min(8).max(200);
 const descripcion = Joi.string().min(15).max(40);
 const dificultad = Joi.string().valid("Alta", "Baja", "Media");
 const fechaInicio = Joi.date().greater("1-1-2023").less(Joi.ref("fechaFinal"));
