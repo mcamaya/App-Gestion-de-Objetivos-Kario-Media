11a525a5501e041a9a4f1746a39622b7fe519fa0
mcamaya <camiamayar@gmail.com>
2023-10-11 19:30:43 -0500

    feat: :sparkles: Alertas bonitas con sweetalert2

--- app/frontend/src/components/metaDashboard.jsx
+++ app/frontend/src/components/metaDashboard.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, Suspense, lazy } from "react";
 import { useHistory } from "react-router-dom";
+import Swal from "sweetalert2";
 import Spinner from "./spinner.jsx";
 import urlApi from "../data/urlApi.js";
 import "./css/metaDashboard.css";
@@ -7,6 +8,15 @@ import "./css/metaDashboard.css";
 const MetasContent = lazy(() => import("./metasContent.jsx"));
 
 export default function MetaDashboard() {
+  /* prueba con sweetalert2 */
+  const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+      confirmButton: "btn btn-success",
+      cancelButton: "btn btn-danger",
+    },
+    buttonsStyling: false,
+  });
+
   const metaId = localStorage.getItem("IDMeta");
   const token = localStorage.getItem("x-auth-token");
   const history = useHistory();
@@ -36,70 +46,93 @@ export default function MetaDashboard() {
     let newTareas = [...tareas];
     let editado = newTareas.find((t) => t._id === tareaId);
     let index = newTareas.findIndex((t) => t._id === tareaId);
-    if (
-      window.confirm(
-        `Seguro que desea marcar la tarea como ${
+
+    swalWithBootstrapButtons
+      .fire({
+        title: `¿Marcar la tarea como ${
           editado.check ? "INCOMPLETA" : "COMPLETADA"
-        }?`
-      )
-    ) {
-      editado["check"] = !editado["check"];
-      newTareas.splice(index, 1, editado);
-      console.log(tareas, newTareas);
-
-      newTareas.forEach((e, i) => {
-        delete newTareas[i].integranteData;
-        delete newTareas[i]._id;
-      });
-
-      fetch(`${urlApi}/metas/add-tasks/${metaId}`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "x-auth-token": token,
-        },
-        body: JSON.stringify({ tareas: newTareas }),
+        }?`,
+        //text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Sí, deseo hacerlo",
+        cancelButtonText: "Mejor no",
+        reverseButtons: true,
       })
-        .then((res) => res.json())
-        .then((data) => console.log(data))
-        .finally(() => window.location.reload())
-        .catch((err) => alert(err));
-    }
+      .then((result) => {
+        if (result.isConfirmed) {
+          editado["check"] = !editado["check"];
+          newTareas.splice(index, 1, editado);
+          console.log(tareas, newTareas);
+
+          newTareas.forEach((e, i) => {
+            delete newTareas[i].integranteData;
+            delete newTareas[i]._id;
+          });
+
+          fetch(`${urlApi}/metas/add-tasks/${metaId}`, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              "x-auth-token": token,
+            },
+            body: JSON.stringify({ tareas: newTareas }),
+          })
+            .then((res) => res.json())
+            .then((data) => console.log(data))
+            .finally(() => window.location.reload())
+            .catch((err) => alert(err));
+
+          swalWithBootstrapButtons.fire(
+            "Hecho!",
+            "Se han guardado los cambios exitosamente"
+          );
+        }
+      });
   };
 
   /* eliminar una tarea */
   const deletingTask = (tareaId) => {
     let newTareas = [...tareas];
     let index = newTareas.findIndex((t) => t._id === tareaId);
-    if (
-      window.confirm(
-        `Seguro que desea marcar eliminar la tarea '${newTareas[index].titulo}'`
-      )
-    ) {
-      newTareas.splice(index, 1);
-      newTareas.forEach((e, i) => {
-        delete newTareas[i].integranteData;
-        delete newTareas[i]._id;
-      });
 
-      console.log(newTareas);
-      fetch(`${urlApi}/metas/add-tasks/${metaId}`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "x-auth-token": token,
-        },
-        body: JSON.stringify({ tareas: newTareas }),
-      })
-        .then((res) => res.json())
-        .then((data) => console.log(data))
-        .finally(() => window.location.reload())
-        .catch((err) => alert(err));
-    }
+    Swal.fire({
+      title: "Estás seguro?",
+      text: "Al eliminar este campo no podrás recuperarlo",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Sí, eliminar!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        newTareas.splice(index, 1);
+        newTareas.forEach((e, i) => {
+          delete newTareas[i].integranteData;
+          delete newTareas[i]._id;
+        });
+
+        console.log(newTareas);
+        fetch(`${urlApi}/metas/add-tasks/${metaId}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "x-auth-token": token,
+          },
+          body: JSON.stringify({ tareas: newTareas }),
+        })
+          .then((res) => res.json())
+          .then((data) => console.log(data))
+          .finally(() => window.location.reload())
+          .catch((err) => alert(err));
+
+        Swal.fire("¡Eliminado!", "El registro ha sido borrado con éxito");
+      }
+    });
   };
 
   const addNewTask = (newTask) => {
-    console.log('entraaa');
+    console.log("entraaa");
     let newTareas = [...tareas];
     newTareas.forEach((e, i) => {
       delete newTareas[i].integranteData;
@@ -120,7 +153,7 @@ export default function MetaDashboard() {
       .then((data) => console.log(data))
       .finally(() => window.location.reload())
       .catch((err) => alert(err));
-  }
+  };
 
   if (!apiData) {
     // esperar a recibir respuesta para renderizar
